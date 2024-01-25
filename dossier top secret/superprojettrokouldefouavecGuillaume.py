6c1e8519dfb77063d14c3f982b424361b3f1b1ca
martgrem <ps28jyt@eduvaud.ch>
2024-01-25 15:06:56 +0100

    poiuzgfbjuzg

--- dossier top secret/superprojettrokouldefouavecGuillaume.py
+++ dossier top secret/superprojettrokouldefouavecGuillaume.py
@@ -8,7 +8,7 @@ lettres = set("qwertzuiopasdfghjklyxcvbnméèêëàäâîìïôöòûüùÿ")
 nb_chances = 10
 hard = False
 nb_mots = 1
-dico = open("liste.de.mots.francais.frgut.txt", "r")
+dico = open("dossier top secret/liste.de.mots.francais.frgut.txt", "r")
 réponses = dico.readlines()
 dico.close()
 
